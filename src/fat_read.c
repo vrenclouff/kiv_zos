45ff9bdf9d9b14801e947f9056adb807f4852147
Lukas <luc.cerny@gmail.com>
2015-12-27 11:05:00 +0100

    Uvolneni pameti + komentare

--- src/fat_read.c
+++ src/fat_read.c
@@ -4,6 +4,11 @@
 
 #include "fat_read.h"
 
+/***********************************************************
+ * Pridani nove root_directory
+ *    root - korenova slozka
+ *    item - slozka pro pridani
+************************************************************/
 void add_root_directory(struct root_dir *root, struct root_directory *item)
 {
   struct root_dir_dyn *root_dir_tmp;
@@ -27,6 +32,12 @@ void add_root_directory(struct root_dir *root, struct root_directory *item)
 
 }
 
+/***********************************************************
+ * Pridani noveho klastru
+ *    cluster - korenovy klastr
+ *    p_cluster - klastr s daty
+ *    position - pozice ve fat tabulce
+************************************************************/
 void add_cluster(struct cluster *cluster, char *p_cluster, unsigned int position)
 {
   struct cluster_dyn *cluster_dyn;
@@ -47,6 +58,9 @@ void add_cluster(struct cluster *cluster, char *p_cluster, unsigned int position
   cluster -> last = cluster_dyn; 
 }
 
+/***********************************************************
+ * Uvolneni pameti
+************************************************************/
 void free_fat(struct boot_record *boot_record, struct root_dir *root_dir, struct fat_table *fat_table, struct cluster *cluster)
 {
   struct root_dir_dyn *root_dir_item;
@@ -75,28 +89,39 @@ void free_fat(struct boot_record *boot_record, struct root_dir *root_dir, struct
   free(root_dir);
 }
 
+/***********************************************************
+ * Nacte dat z fat do struktur
+ *    file_name - jmeno fat souboru
+ *    boot_record - pocatecni data o fat souboru
+ *    root_dir - popis souboru/slozky
+ *    fat_table - fat tabulka
+ *    cluster - clustry fatky
+************************************************************/
 void read_fat(char *file_name, struct boot_record *boot_record, struct root_dir *root_dir, struct fat_table *fat_table, struct cluster *cluster)
 {
-  
   struct root_directory *p_root_directory;      
   unsigned int *fat_item;
-  unsigned int *fat;
   char *p_cluster;
   FILE *p_file;
   int i,j;
-         
+  
+  /* otevre soubor pro cteni */
   p_file = fopen(file_name, "r");
 
+  /* kontrola spravne otevreneho souboru */
   if (!p_file)
   {
     printf("Error open file\n");
     exit(2);
   }
 
+  /* skok na zacatek souboru */
   fseek(p_file, SEEK_SET, 0); 
+  
+  /* nacteni boot_record */
   fread(boot_record, sizeof(struct boot_record), 1, p_file);
 
-
+  /* kontrola spravnosti signatury u nacteneho boot_record */
   if (!strcmp(boot_record -> signature, OK))
   {
     printf("FAT signatura: OK.\n");
@@ -104,31 +129,31 @@ void read_fat(char *file_name, struct boot_record *boot_record, struct root_dir 
   } else if (!strcmp(boot_record -> signature, NOK))
   {
     printf("FAT tabulky nejsou konzistencni.\n");
-    exit(1);
+    exit(2);
   }else if (!strcmp(boot_record -> signature, FAI))
   {
     printf("FAT tabulky obsahuji chyby.\n");
-    exit(1);
+    exit(2);
   }
- 
   
-  fat_item = (unsigned int *) malloc (sizeof (unsigned int));
-  fat = (unsigned int *) calloc(boot_record -> cluster_count, sizeof(unsigned int));
-
+  fat_table -> fat = (unsigned int *) calloc(boot_record -> cluster_count, sizeof(unsigned int));
   fat_table -> size = boot_record -> cluster_count;
-  
+ 
+  /* cteni fat tabulky */
   for (i = 0; i < boot_record -> cluster_count; i++)
   {
-    fread(&fat[i], sizeof(unsigned int), 1, p_file);   
+    fread(&(fat_table -> fat)[i], sizeof(unsigned int), 1, p_file);   
   }
 
+  fat_item = (unsigned int *) malloc (sizeof (unsigned int));
+  
   for (j = 1; j < boot_record -> fat_copies; j++)
   {
     for (i = 0; i < boot_record -> cluster_count; i++)
     {
       fread(fat_item, sizeof(unsigned int), 1, p_file);  
       
-      if (*fat_item != fat[i])
+      if (*fat_item != fat_table -> fat[i])
       {
         printf("Tabulky FAT se neshoduji.\n");
         exit(2);
@@ -138,8 +163,8 @@ void read_fat(char *file_name, struct boot_record *boot_record, struct root_dir 
   free(fat_item);
 
   printf("FAT table check: OK.\n");
-  fat_table -> fat = fat;
 
+  /* cteni root_directory */
   for (i = 0; i < boot_record -> root_directory_max_entries_count; i++)
   {
     p_root_directory = (struct root_directory *) malloc (sizeof (struct root_directory));
@@ -147,17 +172,24 @@ void read_fat(char *file_name, struct boot_record *boot_record, struct root_dir 
     add_root_directory(root_dir, p_root_directory);
   }
 
+  /* cteni klastru */
   for (i = 0; i < boot_record -> reserved_cluster_count; i++)
   {   
     p_cluster = malloc(sizeof(char) * boot_record -> cluster_size);
     fread(p_cluster, sizeof(char) * boot_record -> cluster_size, 1, p_file);
-    if (fat[i] == FAT_UNUSED) continue;
+    if (fat_table -> fat[i] == FAT_UNUSED)
+    {
+      free(p_cluster);
+      continue;
+    }
     add_cluster(cluster, p_cluster, i);
   }
 
+  /* kontrola spravneho poctu nactenych klastru */
   if (cluster -> size == (boot_record -> reserved_cluster_count - boot_record -> root_directory_max_entries_count)) printf("FAT check cluster: OK\n");
   else printf("FAT check cluster: FAIL\n");
 
+  /* uzavreni souboru */
   fclose(p_file);
 }
 
